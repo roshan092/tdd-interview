53246a5512fc25a24f01a1908e2db8230a313c11
Roshan lobo <roshan.lobo@gmail.com>
2017-11-23 18:02:04 +1100

    Add service to controller - with error fixed on application startup

--- src/main/java/com/roshan092/tddinterview/service/UserNameService.java
+++ src/main/java/com/roshan092/tddinterview/service/UserNameService.java
@@ -1,5 +1,8 @@
 package com.roshan092.tddinterview.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserNameService {
     public String getFullNameInUpperCase(String firstName, String lastName) {
         return firstName == null || lastName ==null? "":(firstName + " " + lastName).toUpperCase();
