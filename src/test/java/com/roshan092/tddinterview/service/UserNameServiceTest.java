8f0ecfc8c849dd025d25d06c1798dd8f0b68cf23
Roshan lobo <roshan.lobo@gmail.com>
2017-11-23 21:54:26 +1100

    Mock test example

--- src/test/java/com/roshan092/tddinterview/service/UserNameServiceTest.java
+++ src/test/java/com/roshan092/tddinterview/service/UserNameServiceTest.java
@@ -1,17 +1,28 @@
 package com.roshan092.tddinterview.service;
 
+import com.roshan092.tddinterview.domain.UserDetails;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UserNameServiceTest {
 
+    @InjectMocks
     private UserNameService userNameService;
+    @Mock
+    private ICompanyService companyService;
 
     @Before
     public void setUp() {
-        userNameService = new UserNameService(null);
+        userNameService = new UserNameService(companyService);
     }
 
     @Test
@@ -30,5 +41,19 @@ public class UserNameServiceTest {
     public void shouldReturnEmptyStringWhenLastNameIsNull() throws Exception {
         String result = userNameService.getFullNameInUpperCase("Roshan", null);
         assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    public void shouldReturnUserDetailsForId() throws Exception {
+        when(companyService.getName(1)).thenReturn("CoreLogic");
+        UserDetails userDetails = userNameService.getUserDetails(1);
+        assertThat(userDetails).isEqualTo(
+                UserDetails.builder()
+                        .id(1)
+                        .name("ROSHAN LOBO")
+                        .companyName("CoreLogic")
+                        .build()
+        );
+        verify(companyService).getName(1);
     }
 }
