dd1da3ec8028b107ff8fcc1a5fd11d08f58b2388
Roshan lobo <roshan.lobo@gmail.com>
2017-11-23 17:13:12 +1100

    Add raw code for spring boot project

---
+++ src/main/java/com/roshan092/tddinterview/TddInterviewApplication.java
@@ -0,0 +1,12 @@
+package com.roshan092.tddinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddInterviewApplication.class, args);
+	}
+}
